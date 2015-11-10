5441bfd342b8060800202b18e788375c57422127
Dilraj Singh <dilrajsingh.7@gmail.com>
2015-11-09 18:43:13 -0800

    new Table UI w/refresh & reset functionality
    changed a lot, a lot more usable now

--- js/NFLschedule.js
+++ js/NFLschedule.js
@@ -1,180 +1,145 @@
 var debug = false;
 
 var NFLschedule = (function(){
-
-	var days = [];	 //api gets games in order inside week	
-	var games_by_days = {};
-	var week_number;
 	
-
+	var localJsonObj;
+	var $games = {};
 	var url = 'http://www.nfl.com/liveupdate/scorestrip/ss.xml'
 	var xhr = new XMLHttpRequest();
 
-	function formatData(jsonObj) {
-
-		week_number = jsonObj.gms.w;
-
-		$('#week_number').text("Week " + week_number);
-
-		for (var i = 0; i < jsonObj.gms.g.length; i++) {
-			game = jsonObj.gms.g[i];
-			
-			key = game.d + " @ " + game.t;
-			//console.log(key)
-
-			if(games_by_days[key]) {
-				games_by_days[key].push(game);
-			} else {
-				games_by_days[key] = [game];
-				days.push(key);
-			}
-		}	
-	}
+	//$('#week_number').text("Week " + week_number);
+	//week_number = jsonObj.gms.w;
 
 	function saveLocalData() {
-
-		var local_data = {
-			'w': week_number,
-			'gms': games_by_days,
-			'days': days
-		};
-
-		console.log(days);
-
-		chrome.storage.local.set({'gamesJson': local_data}, 
+		chrome.storage.local.set({'gamesJson': localJsonObj}, 
 			function() {
 				if(debug){console.log('Settings saved');}
 		});
 	}
+	function resetGames() {
+		getNewWeekData(false);	
+	}
 
-	function displayAllDays(jsonObj) {
+	function displayAllDays() {
 
+		var $reset_games = $('#reset_games');
+		$reset_games.click(resetGames);
 
-		// go through all days
-		for (var i = 0; i < days.length; i++) {
-			if(debug){console.log(days[i] + "...");}
+		var $refresh_scores = $('#refresh_scores');
+		$refresh_scores.click(updateGames);
+		
 
-			var $game_div = $('#game_day_template').clone();
-			$game_div.removeAttr('id');
+		$('#week_number').text("Week " + localJsonObj.gms.w);
 
-			var $game_title = $game_div.find('.card-title');
-			var $game_list = $game_div.find('ul');
+		var $game_table = $('#game_table');
 
-			$game_title.html(days[i]);
+		for (var i = 0; i < localJsonObj.gms.g.length; i++) {
 
-			// go through games for that day
-			for (var j = 0; j < games_by_days[days[i]].length; j++) {
+			var $game_item = $game_table.find('#game_item_template').clone();
+			$game_item.removeAttr('id');
 
-				var $game_item = $game_div.find('#game_item_template').clone();
-				$game_item.removeAttr('id');
+			var $home_team = $game_item.find('#home_team');
+			var $away_team = $game_item.find('#away_team');
+			var $home_score = $game_item.find('#score');
+			var $game_time = $game_item.find('#time');
 
-				var $home_team = $game_item.find('#home-team');
-				var $away_team = $game_item.find('#away-team');
+			var game = localJsonObj.gms.g[i];
 
-				var $home_score = $game_item.find('#home-score');
-				var $away_score = $game_item.find('#away-score');
-				
-				var $game_time = $game_item.find('#game-time');
+			$home_team.html(game.hnn);
+			$away_team.html(game.vnn);
 
-				var game = games_by_days[days[i]][j];
+			$home_score.html(game.hs + '-' + game.vs);
 
-				$home_team.html(game.hnn);
-				$away_team.html(game.vnn);
+			var arrtime = game.t.split(':');
+			var hrs = arrtime[0];
+			var mins = arrtime[1];
 
-				$home_score.html(game.hs);
-				$away_score.html(game.vs);
+			/*var date = new Date();
+			date.setHours(parseInt(hrs)+4);
+			date.setMinutes(parseInt(mins));
+			console.log(date.toString() + ' || ' + hrs+":"+mins + ' --> ' + date.toLocaleTimeString() );*/
 
-				var arrtime = game.t.split(':');
-				var hrs = arrtime[0];
-				var mins = arrtime[1];
+			$game_time.html(hrs + ":" + mins /* + " (EST)"*/);
+			
+			$game_table.append($game_item);
+			$game_item.removeAttr('style');
 
-				/*var date = new Date();
-				date.setHours(parseInt(hrs)+4);
-				date.setMinutes(parseInt(mins));
-				console.log(date.toString() + ' || ' + hrs+":"+mins + ' --> ' + date.toLocaleTimeString() );*/
-
-				$game_time.html(hrs + ":" + mins /* + " (EST)"*/);
-				$game_list.append($game_item);
-				$game_item.removeAttr('style');
-				
-
-				var $remove = $game_item.find('#remove');
-				$remove.click({eid: game.eid, game:$game_item, 
-					daytimekey: game.d + " @ " + game.t},removeGame);
-
-				//console.log(game);
-
-				$game_item.attr('id', game.eid);
-			}
-
-			$('#nfl_games').append($game_div);
-			$game_div.show();
-		}
+			$games[game.eid] = $game_item;
+			console.log($games);
+			
+			var $remove = $game_item.find('#remove');
+			$remove.click({eid: game.eid, game:$game_item},removeGame);
+		}		
 	}
 
 	/* 
 		gets the game data for the week if no local data is present, or if 
 		week has been updated 
 	*/
-	function getNewWeekData(localJsonObj) {
+	function getNewWeekData(exists) {
 
 		xhr.open("GET", url, true);		
 		xhr.onreadystatechange = function() {
-			if(debug){console.log('got xml');}
 
 			var response = xhr.responseXML;
 
 			if(response) {
+
 				var jsonObj = $.xml2json(response);
+				console.log('getNewWeekData');
 
-				console.log('doing shit')
-				console.log(localJsonObj)
-				console.log( "==>" + localJsonObj['w']);
+				// same week, don't update
+				if(exists && (localJsonObj.gms.w == jsonObj.gms.w) ) {}
 
-				console.log(localJsonObj['w'] == jsonObj.gms.w);
-
-				// same week, display old data
-				if(  localJsonObj && (localJsonObj['w'] == jsonObj.gms.w) ) {
-
-					games_by_days = localJsonObj['gms'];
-					days = localJsonObj['days'];
-					week_number = localJsonObj['w'];
-
-					console.log(games_by_days);
-					console.log(days);
-					console.log(week_number);
-
-				}
-
-				else {
-					formatData(jsonObj);
-					saveLocalData();
+				else { // new week, update
+					localJsonObj = null;
+					localJsonObj = jsonObj;
 				}	
 
 				displayAllDays();
-				
 			}
 		}
 		xhr.send();
 	}
 
-	function updateGames() {
+	// TODO -- still need $games obj
+	/*
+		<g eid="2015110900" gsis="56634" d="Mon" t="8:30" q="1" k="08:11" 
+		h="SD" hnn="chargers" hs="0" v="CHI" vnn="bears" vs="0" p="SD" rz="1" 
+		ga="" gt="REG"/>
 
-		xhr.open("GET", url, true);
+
+		<g eid="2015110900" gsis="56634" d="Mon" t="8:30" q="1" k="06:50" 
+		h="SD" hnn="chargers" hs="7" v="CHI" vnn="bears" vs="0" p="CHI" rz="0" 
+		ga="" gt="REG"/>
+	*/
+	function updateGames() { 
+
+		xhr.open("GET", url, true);		
 		xhr.onreadystatechange = function() {
+			
+			var response = xhr.responseXML;
+			if(response) {
+				var jsonObj = $.xml2json(response);
 
-			for (var i = 0; i < result.gms.g.length; i++) {
-				game = jsonObj.gms.g[i];
-				
-				key = game.eid;
+				console.log('updating...');
+				for (var i = 0; i < jsonObj.gms.g.length; i++) {
+					
+					var game = jsonObj.gms.g[i];
+					var q = game.q;
 
+					if (q == '1' || q == '2' || q == '3' || q == '4' ) { 
+						console.log('changing scores.. ' + game.hs + '-' + game.vs);
+						var $scores = $games[game.eid].find('#score');
+						$scores.html(game.hs + '-' + game.vs);
 
-				if (game.q == 'iP') { //TODO
-					var $home_score = $games[key].find('#home-score');
-					var $away_score = $games[key].find('#away-score');
-
-					$home_score.html(game.hs);
-					$away_score.html(game.vs);
+						for (var j = 0; j < localJsonObj.gms.g.length; j++) {
+							if(localJsonObj.gms.g[j].eid == game.eid) {
+								localJsonObj.gms.g[j] = game;
+								saveLocalData();
+							}
+						}
+					}
 				}
 			}
 		}
@@ -183,27 +148,17 @@ var NFLschedule = (function(){
 
 	function removeGame(event) {
 		console.log('removeGame');
-		//console.log(event.data.eid);
-		//console.log(event.data.game);
-		//console.log(event.data.daytimekey);
 
-		daytimekey = event.data.daytimekey;
-
-		if(daytimekey in games_by_days) {
-			for (var i = 0; i < games_by_days[daytimekey].length; i++) {
-				console.log(games_by_days[daytimekey][i].eid)
-				if(games_by_days[daytimekey][i].eid == event.data.eid) {
-					games_by_days[daytimekey].splice(i, 1);
-					break;
-				}
+		for (var i = 0; i < localJsonObj.gms.g.length; i++) {
+			if(localJsonObj.gms.g[i].eid == event.data.eid) 
+			{
+				localJsonObj.gms.g.splice(i, 1);
+				break;
 			}
-		} 
-
+		}
 		event.data.game.remove();
-
 		saveLocalData();
 	}
-
 
 	function handler() {
 
@@ -212,14 +167,15 @@ var NFLschedule = (function(){
 			if(debug){console.log('handler...');}
 
 			if(result.gamesJson) {
-				getNewWeekData(result.gamesJson);
+				localJsonObj = null;
+				localJsonObj = result.gamesJson;
+				getNewWeekData(true);
+
 			}
 			else {
-				getNewWeekData(null);
+				getNewWeekData(false);
 			}
 		});
-
-
 	}
 
 	/* API for other js code */
