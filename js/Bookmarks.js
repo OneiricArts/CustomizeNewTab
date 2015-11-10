1f07be7d4b9c05f4aaa5759c3fdc8d197aa7b87c
Dilraj Singh <dilrajsingh.7@gmail.com>
2015-11-10 12:17:49 -0800

    links save state
    links work, but duct-taped together. go over again later

--- js/Bookmarks.js
+++ js/Bookmarks.js
@@ -3,22 +3,82 @@ var debug = true;
 var Bookmarks = (function(){
 	
 	$( document ).ready(function() {
-		var $addBookmark = $('#addBookmark');
-		console.log($addBookmark);
-		$addBookmark.click(addBookmark);
+		displayLocal();
+		addHooks();
+		console.log('test');
+
 	});
 	
 	function addBookmark() {
 		var $bookmark_list = $('#bookmark-list');
+
 		console.log($bookmark_list);
 		
-		console.log('test');
-
 		var bookmark = "<a href='" + $('#bookmark_url').val() + 
 		"' class='list-group-item'>" + $('#bookmark_name').val() + "</a>"
+		
 		$bookmark_list.append(bookmark);
+		saveLocal();
 	}
 
+	function editBookmarks() {
+
+		console.log($('#bookmark-list a'));
+		if($(this).hasClass("btn-danger")){ // was in edit mode, remove
+			$('#bookmark-list a').unbind("click"); // will remove preventDefault and removeLink
+			$(this).toggleClass('btn-danger',false);
+		} 
+
+		else{
+			$( '#bookmark-list a' ).click(function( event ) {
+				event.preventDefault();
+			});
+
+			$('#bookmark-list a').bind("click", removeLink);
+			$(this).toggleClass('btn-danger',true);
+		}
+		saveLocal();
+	}
+
+	function removeLink() {
+		$(this).remove();
+		saveLocal();
+	}
+
+	function saveLocal() {
+
+		var $list = $('#bookmark-list').html();
+		console.log($list);
+		chrome.storage.local.set({'localBookmarks': $list}, 
+			function() {
+				if(debug){console.log('Settings saved');}
+		});
+	}
+
+
+	function displayLocal() {
+		chrome.storage.local.get('localBookmarks', function (result) {
+			
+			if(debug){console.log('handler...');}
+
+			// if (false && result.localBookmarks) will reset
+			if(result.localBookmarks) {
+				$('#bookmark-list').html("");
+				$('#bookmark-list').append(result.localBookmarks);
+			}
+
+			saveLocal();
+
+		});
+	}
+
+	function addHooks(){
+		var $addBookmark = $('#addBookmark');
+		$addBookmark.click(addBookmark);
+
+		var $editBookmarks = $('#editBookmarks');
+		$editBookmarks.click(editBookmarks);
+	}
 	/* API for other js code */
 	return {
 		//handler: handler,
