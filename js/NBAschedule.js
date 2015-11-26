881746843c031cbfb308025404fe87cf174eaa28
Dilraj Singh <dilrajsingh.7@gmail.com>
2015-11-26 03:14:28 -0800

    started Handlebars.js use
    stuff is broken, but fundamentals work

--- js/NBAschedule.js
+++ js/NBAschedule.js
@@ -17,7 +17,7 @@ var NBAschedule = (function(){
 	var yyyy = today.getFullYear();
 
 	//dd = '09';
-	//dd = '11';
+	dd = '25';
 	//var url = 'http://data.nba.com/json/cms/noseason/scoreboard/' 
 
 	var url = 'http://data.nba.com/5s/json/cms/noseason/scoreboard/' 
@@ -38,6 +38,12 @@ var NBAschedule = (function(){
 		}
 	}
 
+	function cacheButtons() {
+		var $reset_games = $('#NBA-panel #reset_games');
+		$reset_games.click(resetGames);
+		$('#NBA-panel #standings-btn').click(standings);
+	}
+	
 	function displayAllDays(reset) {
 
 		if(reset) {
@@ -46,61 +52,20 @@ var NBAschedule = (function(){
 		
 		var $game_table = $('#NBA-panel #NBA_game_table');
 
-		var $reset_games = $('#NBA-panel #reset_games');
-		$reset_games.click(resetGames);
 
-		$('#NBA-panel #standings-btn').click(standings);
-
-		//console.log(localJsonObj.sports_content.games.game);
 
 		if(localJsonObj.sports_content.games.game.length == 0) {
 			$game_table.html('<span class="glyphicon glyphicon-bell"></span> No Games Today');
 			return;
 		}
 
-		for (var i = 0; i < localJsonObj.sports_content.games.game.length; i++) {
-
-			var game = localJsonObj.sports_content.games.game[i];
-
-			var $game_item = $game_table.find('#game_item_template').clone();
-			$game_item.removeAttr('id');
-
-
-
-			var $home_team = $game_item.find('#home_team');
-			var $away_team = $game_item.find('#away_team');
-			var $home_score = $game_item.find('#score');
-			var $game_time = $game_item.find('#time');
-
-
-			$home_team.html(game.home.nickname);
-			$away_team.html(game.visitor.nickname);
-
-
-			$home_score.html(game.home.score + '-' + game.visitor.score);
-			
-			//$game_time.html(game.time-1200-300);
-			$game_time.html(game.period_time.period_status + " " + game.period_time.game_clock);
-
-			$game_table.append($game_item);
-			$game_item.show();	
-
-			$games[game.id] = $game_item;
-
-			styleScores(game);
-			
-			/* if i want teh popup to be a row right under:
-			$game_table.append("<tr id='"  
-				+ game.id 
-				+ '#demo'
-				+ "' class='collapse'><td colspan='5'><span id='gameid'></td></tr>")	
-			$game_item.attr('data-target', '#' + game.id);*/
-			$game_item.click({game: game}, createGameDetails);
-
-
-			var $remove = $game_item.find('#remove');
-			$remove.click({id: game.id},removeGame);
-		}	
+		else {
+			var source = document.getElementById("animalTemplate").innerHTML;
+			var template = Handlebars.compile(source);
+			var data = {games: localJsonObj.sports_content.games.game};
+			var output = template(data);
+			$game_table.find('tbody').html(output);
+		}
 
 		updateScores();
 	}
@@ -299,7 +264,7 @@ var NBAschedule = (function(){
 
 
 		$.getJSON(url, function(data) {
-			if(date && date == 
+			if(false && date && date == 
 				data.sports_content.sports_meta.season_meta.calendar_date){
 			}
 			else {
