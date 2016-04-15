4a69416d3fce081aab1868757a5ee4f333fe484c
Dilraj Singh <dilrajsingh.7@gmail.com>
2016-04-15 02:16:56 -0700

    mob remove

--- source/js/NHL.js
+++ source/js/NHL.js
@@ -312,12 +312,29 @@ class MLB extends Sport {
 	}
 
 	cacheButtonActions() {
-		//var that = this;
-		//$('body').on('click', '#NBA_game_table #remove-game-btn', {that: that}, this.removeGame);
+		var that = this;
+		$('body').on('click', '#MLB_widget #remove-game-btn', {that: that}, this.removeGame);
 		$('body').on('click', '#MLB_widget #reset_games', this.resetSchedule.bind(this));
 		$('body').on('click', '#MLB_widget #tomorrow-btn', this.changeDay.bind(this, 1));
 		$('body').on('click', '#MLB_widget #yesterday-btn', this.changeDay.bind(this,-1));
 		$('body').on('click', '#MLB_widget #today-btn', this.changeDay.bind(this,0));
+	}
+
+	removeGame(event) {
+		var that = event.data.that;
+		var id = $(this).closest('tr').attr('id');
+
+		$('#'+id).remove();
+		//$('#c'+id).remove();
+
+		for (var i = 0; i < that.data.data.games.game.length; i++) {
+			if(id == that.data.data.games.game[i].game_pk) {
+				that.data.data.games.game[i]['hidden'] = true;
+				break;
+			}
+		};
+
+		that.saveData();
 	}
 }
 
