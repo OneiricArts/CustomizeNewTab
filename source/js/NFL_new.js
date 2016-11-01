156686ff6868e4763d98b1d1efcd949b2800daf0
Dilraj Singh <dilrajsingh.7@gmail.com>
2016-11-01 16:01:55 -0700

    NFL ESLint
    the cleanup begins

--- source/js/NFL_new.js
+++ source/js/NFL_new.js
@@ -1,122 +1,117 @@
 
-"use strict";
-
 class NFL extends Sport {
 
-	constructor() {
-		super();
-		this.dataKey = 'NFL';
-		this.schedule_url =  'http://www.nfl.com/liveupdate/scorestrip/ss.json';
+  constructor() {
+    super();
+    this.dataKey = 'NFL';
+    this.schedule_url = 'http://www.nfl.com/liveupdate/scorestrip/ss.json';
+  }
+
+  writeToTemplate(error = false) {
+    if (error) {
+      if (!this.data) { this.data = {}; }
+      this.data.error = true;
+      this.data.games = null;
     }
 
-	writeToTemplate(error=false) {
+    this.displayTemplate('NFLschedule', 'schedule', this.data, $('#NFL_widget'));
+  }
 
-		if(error) {
-			if(!this.data) {this.data = {};}
-			this.data['error'] = true;
-			this.data.games = null;
-		}
+  getJsonData(url, callback) {
+    $.getJSON(url, (result) => {
+      this.massageData.call(this, result, callback);
+    }).fail((result) => {
+    });
+    // TODO handle timeout
+  }
 
-        this.displayTemplate('NFLschedule', 'schedule', this.data, $('#NFL_widget'));
-	}
+  displaySchedule(newData) {
+    const updateNewData = newData;
 
-	getJsonData(url, callback) {
+    // if data is for the same week, carry over any data that I need to
+    if (this.data && this.data.gms && this.data.w === updateNewData.w) {
+      for (let i = 0; i < updateNewData.gms.length && this.data.gms.length; i += 1) {
+        if (updateNewData.gms[i].eid === this.data.gms[i].eid && this.data.gms[i].hidden) {
+          updateNewData.gms[i].hidden = true;
+        }
+      }
+    }
 
-		$.getJSON(url, function(result) {
-			this.massageData.call(this, result, callback);
-		}.bind(this)).fail(function(result){
-		}.bind(this));
-		// TODO handle timeout
-	}
+    this.data = updateNewData;
+    this.saveData(this.writeScheduleToDOM());
+  }
 
-	displaySchedule(newData) {
+  massageData(data, callback) {
+    try {
+      const url = 'http://www.nfl.com/liveupdate/scores/scores.json';
+      $.getJSON(url, (result) => {
+        const combinedData = data;
 
-		// if data is for the same week, carry over any data that I need to
-		if(this.data && this.data.gms && this.data.w == newData.w) {
-			for (var i = 0; i < newData.gms.length && this.data.gms.length; i++) {
-				if(newData.gms[i].eid == this.data.gms[i].eid && this.data.gms[i].hidden) {
-					newData.gms[i]['hidden'] = true;
-				}
-			}
-		}
+        for (let i = 0; i < combinedData.gms.length; i += 1) {
+          combinedData.gms[i].extrainfo = result[combinedData.gms[i].eid];
 
-		this.data = newData;
-		this.saveData(this.writeScheduleToDOM());
-	}
+          if (combinedData.gms[i].extrainfo.home.score[1] !== null) {
+            combinedData.gms[i].scoreTable = true;
+          }
 
-	massageData(data, callback) {
-		try {
-            var url = 'http://www.nfl.com/liveupdate/scores/scores.json'
-            $.getJSON(url, function(result) {
-                for (var i = 0; i < data.gms.length; i++) {
-					
-                    data.gms[i]['extrainfo'] = result[data.gms[i].eid];
+          if (!isNaN(combinedData.gms[i].q)) {
+            combinedData.gms[i].playing = true;
+          }
 
-                    if (data.gms[i]['extrainfo'].home.score[1] !== null) {
-                        data.gms[i]['scoreTable'] = true;
-                    }
+          // local time
+          combinedData.gms[i].t = this.toLocalTime(combinedData.gms[i].t.split(':')[0],
+            combinedData.gms[i].t.split(':')[1]).split(' ')[0];
 
-                    if (!isNaN(data.gms[i].q)) {
-                        data.gms[i]['playing'] = true;
-                    }
+          // label whos winning
+          if (combinedData.gms[i].extrainfo.home.score.T !== null) {
+            const homeScore = parseInt(combinedData.gms[i].extrainfo.home.score.T, 10);
+            const visitorScore = parseInt(combinedData.gms[i].extrainfo.away.score.T, 10);
 
-					// local time 
-					data.gms[i].t = this.toLocalTime(data.gms[i].t.split(':')[0], 
-						data.gms[i].t.split(':')[1]).split(' ')[0];
+            if (homeScore > visitorScore) {
+              combinedData.gms[i].home_winning = true;
+            }
+            if (visitorScore > homeScore) {
+              combinedData.gms[i].visitor_winning = true;
+            }
+          }
 
-					// label whos winning
-					if (data.gms[i].extrainfo.home.score.T !== null) {
-						var home_score = parseInt(data.gms[i].extrainfo.home.score.T);
-						var visitor_score = parseInt(data.gms[i].extrainfo.away.score.T);
+          // who has possession
+          if (combinedData.gms[i].playing &&
+            combinedData.gms[i].extrainfo.posteam === combinedData.gms[i].extrainfo.home.abbr) {
+            combinedData.gms[i].home_pos = true;
+          } else if (combinedData.gms[i].playing &&
+            combinedData.gms[i].extrainfo.posteam === combinedData.gms[i].extrainfo.away.abbr) {
+            combinedData.gms[i].visitor_pos = true;
+          }
+        }
+        callback.call(this, combinedData);
+      });
+    } catch (e) {
+      console.log(`fudge cakes -- massageData${e}`);
+    }
+  }
 
-						if (home_score > visitor_score) {
-							data.gms[i]['home_winning'] = true;
-						}
-						if (visitor_score > home_score) {
-							data.gms[i]['visitor_winning'] = true;
-						}
-					}
+  cacheButtonActions() {
+    const self = this;
+    $('body').on('click', '#NFL-schedule-table #remove-game-btn', { self }, this.removeGame);
+    $('body').on('click', '#NFL_widget #reset_games', this.resetSchedule.bind(this));
+    $('body').on('click', '#NFL_widget #refresh_scores', this.updateSchedule.bind(this));
+  }
 
-					// who has possession
-					if(data.gms[i].playing &&
-						data.gms[i].extrainfo.posteam == data.gms[i].extrainfo.home.abbr) {
-						data.gms[i]['home_pos'] = true;
-					} else if( data.gms[i].playing &&
-						data.gms[i].extrainfo.posteam == data.gms[i].extrainfo.away.abbr) {
-						data.gms[i]['visitor_pos'] = true;
-					}
-                }
-                callback.call(this,data);		
-            }.bind(this));
-		}
-		catch(e) {
-			console.log('fudge cakes -- massageData' + e);
-		}
-	}
+  removeGame(event) {
+    const self = event.data.self;
+    const id = $(this).closest('tr').attr('id');
 
-	cacheButtonActions() {
-		var self = this;
-		$('body').on('click', '#NFL-schedule-table #remove-game-btn', {self: self}, this.removeGame);
-		$('body').on('click', '#NFL_widget #reset_games', this.resetSchedule.bind(this));
-		$('body').on('click', '#NFL_widget #refresh_scores', this.updateSchedule.bind(this));
-	}
+    $(`#${id}`).remove();
+    $(`#c${id}`).remove();
 
-	removeGame(event) {
-		var self = event.data.self;
-		var id = $(this).closest('tr').attr('id');
+    for (let i = 0; i < self.data.gms.length; i += 1) {
+      if (self.data.gms[i].eid == id) { // === results in bug
+        self.data.gms[i].hidden = true;
+        break;
+      }
+    }
 
-		$('#'+id).remove();
-		$('#c'+id).remove();
-
-		for (var i = 0; i < self.data.gms.length; i++) {
-			if(self.data.gms[i].eid == id) {
-				self.data.gms[i]['hidden'] = true;
-				break;
-			}
-		}
-
-		self.saveData();
-	}
+    self.saveData();
+  }
 }
-
-
