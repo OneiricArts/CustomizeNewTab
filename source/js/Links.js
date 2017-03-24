885cbb906f6aea4f410db2c90f80e072d7be2145
Dilraj Singh <dilrajsingh.7@gmail.com>
2017-03-24 03:57:27 -0700

    Links.js cleanup part 1. ESLint
    Superficial cleanup: passes eslint
    Renamed that to self

--- source/js/Links.js
+++ source/js/Links.js
@@ -1,92 +1,81 @@
-function Links() {
-	Base.call(this);
+class Links extends Base { // eslint-disable-line no-unused-vars
 
-   this.datakey = "Links_data";
+  constructor() {
+    super();
+    this.datakey = 'Links_data';
+    this.loadData(this.showTopSites, this.setDefaults);
+  }
 
-   this.loadData(
-      this.showTopSites,
-      //this.setDefaults,
-      this.setDefaults
-   );
-};
+  init() {
+    const self = this;
 
-Links.prototype = Object.create(Base.prototype); // See note below
-Links.prototype.constructor = Links;
+    $('#bookmark-panel').on('click', '#remove_customLink', (evt) => {
+      evt.preventDefault();
+      self.removeCustomLink($(this).closest('a'));
+    });
 
-Links.prototype.init = function(){   
-   var that = this;
-   
-   $("#bookmark-panel").on('click', '#remove_customLink', function(evt) {
-      evt.preventDefault(); 
-      that.removeCustomLink($(this).closest('a'));
-   });
+    // $("body").on('click', '#bookmark-panel #remove', {self:this}, this.removeCustomLink);
+    $('body').on('click', '#addBookmark', { self: this }, this.addBookmark);
 
-   //$("body").on('click', '#bookmark-panel #remove', {that:this}, this.removeCustomLink);
-   $("body").on('click', '#addBookmark', {that:this}, this.addBookmark);
-
-   /* clear values on close */
-   $('body').on('hidden.bs.modal', '#addCustomLinkModal', function () {
+    /* clear values on close */
+    $('body').on('hidden.bs.modal', '#addCustomLinkModal', () => {
       $('#addCustomLinkModal .modal-body').find('textarea,input').val('');
-   });
-};
+    });
+  }
 
-Links.prototype.addBookmark = function(event){
-   var that = event.data.that;
-   that.data.push({
-         title: $('#bookmark_name').val(),
-         url: $('#bookmark_url').val()
-      });
-   that.saveData(that.showTopSites());
-};
+  addBookmark(event) { // eslint-disable-line class-methods-use-this
+    const self = event.data.self;
+    self.data.push({
+      title: $('#bookmark_name').val(),
+      url: $('#bookmark_url').val(),
+    });
+    self.saveData(self.showTopSites());
+  }
 
-Links.prototype.showTopSites = function(){
-   chrome.topSites.get(function(mostVisitedURLs){
-     
-      //var $template = $("#Links-template").html();
-      var dataKey = "links";
-      var dataObj = {};
+  showTopSites() {
+    chrome.topSites.get((mostVisitedURLs) => {
+        // var $template = $("#Links-template").html();
+      const dataKey = 'links';
+      const dataObj = {};
 
-      dataObj['topSites'] = mostVisitedURLs.slice(0,5);
+      dataObj.topSites = mostVisitedURLs.slice(0, 5);
 
-      if(this.data && this.data.length > 0) {
-         dataObj['customSites'] = this.data;
+      if (this.data && this.data.length > 0) {
+        dataObj.customSites = this.data;
       }
 
-      var $element = $("#bookmark-panel");
+      const $element = $('#bookmark-panel');
       this.displayTemplate('Links', dataKey, dataObj, $element);
       this.saveData();
-   }.bind(this));
-};
+    });
+  }
 
-Links.prototype.setDefaults = function() {
-
-   this.data = [
+  setDefaults() {
+    this.data = [
       {
-         title: 'NPR',
-         url: 'http://www.npr.org/'
+        title: 'NPR',
+        url: 'http://www.npr.org/',
       },
       {
-         title: 'UNICEF',
-         url: 'http://www.unicef.org/'
+        title: 'UNICEF',
+        url: 'http://www.unicef.org/',
       },
       {
-         title: 'Hints',
-         url: 'https://github.com/OneiricArts/CustomizeNewTab/wiki/Hints'
-      }
-      
-   ];
-   this.showTopSites();
-};
+        title: 'Hints',
+        url: 'https://github.com/OneiricArts/CustomizeNewTab/wiki/Hints',
+      },
+    ];
+    this.showTopSites();
+  }
 
-Links.prototype.removeCustomLink = function($el) {
-   
-   this.data.splice(parseInt($el.attr('id').split('_')[0]),1);
-   
-   if(this.data.length == 0) {
+  removeCustomLink($el) {
+    this.data.splice(parseInt($el.attr('id').split('_')[0], 10), 1);
+
+    if (this.data.length === 0) {
       $('#custom-sites').remove();
       this.saveData();
-   }
-   else {
+    } else {
       this.saveData(this.showTopSites);
-   }
-};
+    }
+  }
+}
