b4df276146713040790ce410d1a1654ffc7e2fc9
Dilraj Singh <dilrajsingh.7@gmail.com>
2017-03-30 11:10:48 -0700

    Commit .eslintrc.js
    Should’ve had this since the beginning. Code style/guidelines should be
    integral to repo.

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "plugins": [
+    "import"
+  ],
+  "env": {
+    "browser": true,
+    "es6": true,
+    "jquery": true,
+    "webextensions": true
+  },
+  "globals": {
+    "Handlebars": false,
+    "devEnv": false,
+    "log": false,
+    "gaSendEvent": false,
+    "Widget": false,
+    "Sport": false,
+    "NBA": false,
+    "NFL": false,
+    "NHL": false,
+    "MLB": false,
+    "Links": false,
+    // temp:
+    "Base": false
+  },
+  "rules": {
+  }
+};
