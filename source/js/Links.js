b49875c2cca38a9d8743d8fc577584a65dd34add
Dilraj Singh <dilrajsingh.7@gmail.com>
2017-04-04 09:26:58 -0700

    Links rewrite :smile:
    Now awesomely simple!
    Uses new widget class.
    Much better organized.
    Fixes bug #4
    
    Closes #8 #4

--- source/js/Links.js
+++ source/js/Links.js
@@ -1,57 +1,94 @@
-class Links extends Base { // eslint-disable-line no-unused-vars
+/**
+ * Links widget: my links and Chrome Top Sites
+ * @extends widgetNew
+ * @requires jQuery.js
+ */
+class Links extends WidgetNew { // eslint-disable-line no-unused-vars
 
   constructor() {
     super();
     this.datakey = 'Links_data';
-    this.loadData(this.showTopSites, this.setDefaults);
   }
 
-  init() {
-    const self = this;
+  async init() {
+    await this.loadData();
 
-    $('#bookmark-panel').on('click', '#remove_customLink', (evt) => {
-      evt.preventDefault();
-      self.removeCustomLink($(this).closest('a'));
-    });
+    /*
+      BACKWARD COMPATIBILITY: CARRY OVER USER DATA
+      Used to store this.data = [custom links array]
+      check if its still an array and carry over the data
 
-    // $("body").on('click', '#bookmark-panel #remove', {self:this}, this.removeCustomLink);
-    $('body').on('click', '#addBookmark', { self: this }, this.addBookmark);
+      also, if it is an array, need to change it to an object
+      @TODO
+    */
+    if (Object.prototype.toString.call(this.data) === '[object Array]') {
+      const arr = this.data;
+      this.data = {};
+      this.data.myLinks = arr;
+    }
 
-    /* clear values on close */
-    $('body').on('hidden.bs.modal', '#addCustomLinkModal', () => {
-      $('#addCustomLinkModal .modal-body').find('textarea,input').val('');
-    });
-  }
-
-  addBookmark(event) { // eslint-disable-line class-methods-use-this
-    const self = event.data.self;
-    self.data.push({
-      title: $('#bookmark_name').val(),
-      url: $('#bookmark_url').val(),
-    });
-    self.saveData(self.showTopSites());
-  }
-
-  showTopSites() {
-    chrome.topSites.get((mostVisitedURLs) => {
-        // var $template = $("#Links-template").html();
-      const dataKey = 'links';
-      const dataObj = {};
-
-      dataObj.topSites = mostVisitedURLs.slice(0, 5);
-
-      if (this.data && this.data.length > 0) {
-        dataObj.customSites = this.data;
-      }
-
-      const $element = $('#bookmark-panel');
-      this.displayTemplate('Links', dataKey, dataObj, $element);
+    if (!this.data.myLinks) {
+      this.setMyLinksDefaults();
       this.saveData();
+    }
+
+    this.showLinks();
+
+    /**
+     * Click handlers
+     */
+
+    $('#Links_widget').on('click', '#remove-my-link', e => this.removeMyLink(e));
+    $('body').on('click', '#add-my-link-submit', () => this.addMyLink());
+
+    // add-my-link modal: clear values on close
+    $('body').on('hidden.bs.modal', '#add-my-link-modal', () => {
+      $('#add-my-link-modal .modal-body').find('textarea,input').val('');
     });
   }
 
-  setDefaults() {
-    this.data = [
+  async showLinks() {
+    const mostVisitedURLs = await browser.getTopSites();
+    const dataObj = {
+      topSites: mostVisitedURLs.slice(0, 5),
+      myLinks: this.data.myLinks,
+    };
+
+    WidgetNew.displayTemplate('Links', 'links', dataObj, $('#Links_widget'));
+  }
+
+  addMyLink() {
+    this.data.myLinks.push({
+      title: $('#add-my-link-name').val(),
+      url: $('#add-my-link-url').val(),
+    });
+    this.showLinks(); // to show updated list
+    this.saveData();
+  }
+
+  removeMyLink(e) {
+    e.preventDefault(); // don't trigger/follow link
+    const $el = $(e.currentTarget).closest('a');
+
+    this.data.myLinks.splice(parseInt($el.data('index'), 10), 1);
+    this.showLinks(); // to show updated list
+    this.saveData();
+  }
+
+  setMyLinksDefaults() {
+    this.data.myLinks = [
+      {
+        title: 'To remove a link, press the x ---> ',
+        url: 'https://github.com/OneiricArts/CustomizeNewTab/wiki/Hints',
+      },
+      {
+        title: 'To add your own link, press \'Add Link\'',
+        url: 'https://github.com/OneiricArts/CustomizeNewTab/wiki/Hints',
+      },
+      {
+        title: 'How to use this extension',
+        url: 'https://github.com/OneiricArts/CustomizeNewTab/wiki/Hints',
+      },
       {
         title: 'NPR',
         url: 'http://www.npr.org/',
@@ -60,22 +97,6 @@ class Links extends Base { // eslint-disable-line no-unused-vars
         title: 'UNICEF',
         url: 'http://www.unicef.org/',
       },
-      {
-        title: 'Hints',
-        url: 'https://github.com/OneiricArts/CustomizeNewTab/wiki/Hints',
-      },
     ];
-    this.showTopSites();
-  }
-
-  removeCustomLink($el) {
-    this.data.splice(parseInt($el.attr('id').split('_')[0], 10), 1);
-
-    if (this.data.length === 0) {
-      $('#custom-sites').remove();
-      this.saveData();
-    } else {
-      this.saveData(this.showTopSites);
-    }
   }
 }
