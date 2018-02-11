2e5c6cb1f973b124aa7f65cbd747603498e4f515
Dilraj Singh <dilrajsingh.7@gmail.com>
2018-02-11 12:06:18 -0800

    sport.js tabs to spaces

--- source/js/sport.js
+++ source/js/sport.js
@@ -2,101 +2,101 @@
 
 class Sport extends Widget {
 
-	constructor() {
-		super();
-		this.schedule_url_start;
-		this.schedule_url_end;
-		this.schedule_url;
-		this.today = new Date();
-	}
+  constructor() {
+    super();
+    this.schedule_url_start;
+    this.schedule_url_end;
+    this.schedule_url;
+    this.today = new Date();
+  }
 
-	init() {
-		this.loadLocalSchedule();
-		this.cacheButtonActions();
-		this.specificInit();
-	}
+  init() {
+    this.loadLocalSchedule();
+    this.cacheButtonActions();
+    this.specificInit();
+  }
 
-	/*
-		OVERWRITE in extending classes for unique functionality
-	*/
-	specificInit() {}
-	cacheButtonActions() {}
-	writeToTemplate() {}
-	dataOutOfDate(newData) {return true;}
-	updateNewData(newData) {}
-	massageData(data, callback) {callback.call(this, data);}
-	highlightGames() {}
-	formatDate() {}
+  /*
+    OVERWRITE in extending classes for unique functionality
+  */
+  specificInit() {}
+  cacheButtonActions() {}
+  writeToTemplate() {}
+  dataOutOfDate(newData) {return true;}
+  updateNewData(newData) {}
+  massageData(data, callback) {callback.call(this, data);}
+  highlightGames() {}
+  formatDate() {}
 
-	yyyymmdd(changeDay) {
-		if(changeDay) {
-			this.today.setDate(this.today.getDate() + changeDay); 
-		}
-		else {
-			this.today = new Date();
-		}
+  yyyymmdd(changeDay) {
+    if(changeDay) {
+      this.today.setDate(this.today.getDate() + changeDay);
+    }
+    else {
+      this.today = new Date();
+    }
 
-		function twoDigits(n) {
-			return n<10? '0'+n:''+n
-		}
+    function twoDigits(n) {
+      return n<10? '0'+n:''+n
+    }
 
-		var string = this.formatDate( 
-				this.today.getFullYear(), 
-				twoDigits(this.today.getMonth()+1), 
-				twoDigits(this.today.getDate())
-			);
+    var string = this.formatDate(
+        this.today.getFullYear(),
+        twoDigits(this.today.getMonth()+1),
+        twoDigits(this.today.getDate())
+      );
 
-		return string;
-	}
+    return string;
+  }
 
-	changeDay(n) {
-		this.schedule_url = this.schedule_url_start + this.yyyymmdd(n) + this.schedule_url_end;
-		this.resetSchedule();
-	}
+  changeDay(n) {
+    this.schedule_url = this.schedule_url_start + this.yyyymmdd(n) + this.schedule_url_end;
+    this.resetSchedule();
+  }
 
-	getJsonData(url, callback) {
-		$.getJSON(url, function(result) {
-			this.massageData.call(this, result, callback);
-		}.bind(this))
-		.fail(function(result){
-			//this.massageData.call(this, result, callback);
-		}.bind(this));
-		// TODO handle timeout
-	}
+  getJsonData(url, callback) {
+    $.getJSON(url, function(result) {
+      this.massageData.call(this, result, callback);
+    }.bind(this))
+    .fail(function(result){
+      //this.massageData.call(this, result, callback);
+    }.bind(this));
+    // TODO handle timeout
+  }
 
-	getDataSchedule () {
-		this.getJsonData(this.schedule_url, this.displaySchedule);
-	}
+  getDataSchedule () {
+    this.getJsonData(this.schedule_url, this.displaySchedule);
+  }
 
-	loadLocalSchedule() {
-		this.loadData( function(){
-			this.writeScheduleToDOM();
-			this.getDataSchedule();
-		}, 
-		this.getDataSchedule);
-	}
+  loadLocalSchedule() {
+    this.loadData( function(){
+      this.writeScheduleToDOM();
+      this.getDataSchedule();
+    },
+    this.getDataSchedule);
+  }
 
-	displaySchedule(newData) {
-		this.updateNewData(newData);
-		this.data = newData;
-		this.saveData(this.writeScheduleToDOM());
-	}
+  displaySchedule(newData) {
+    this.updateNewData(newData);
+    this.data = newData;
+    this.saveData(this.writeScheduleToDOM());
+  }
 
-	writeScheduleToDOM() {
-		this.writeToTemplate();
-		this.highlightGames();
-	}
+  writeScheduleToDOM() {
+    this.writeToTemplate();
+    this.highlightGames();
+  }
 
-	resetSchedule() {
-		this.data = null;
-		this.saveData(this.getDataSchedule);
-	}
+  resetSchedule() {
+    this.data = null;
+    this.saveData(this.getDataSchedule);
+  }
 
-	updateSchedule() {
-		this.getDataSchedule();
-	}
+  updateSchedule() {
+    this.getDataSchedule();
+  }
 
-	writeErrorMessage() {
-		this.writeToTemplate(true);
-	}
+  writeErrorMessage() {
+    this.writeToTemplate(true);
+  }
 }
